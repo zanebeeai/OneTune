cc29c8634ee12bd322c26c01c9ee9dee5ce74506
zanzilla22 <69683242+zanzilla22@users.noreply.github.com>
2023-01-10 22:28:33 -0500

    Add files via upload

---
+++ spotifyConstants.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 23 14:21:46 2020
+
+@author: olehe
+
+Fill in your API info here, and rename to spotifyConstants.py
+"""
+
+myUser = 'zanzilla22'
+myClientID = '0f688660285e40e6acb7df830b0663a6'
+myClientSecret = '0dbdfd0bc1964293bc8f1d114a9511c3'
+myRedirect = 'https://example.com/callback'
